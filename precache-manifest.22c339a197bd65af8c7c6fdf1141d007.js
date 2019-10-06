55bfc372abf8a135c5ab25d0dfb625af5d44247a
ilyakolesnikov <superbobor@bk.ru>
2019-10-06 16:12:36 +0300

    Updates

---
+++ precache-manifest.22c339a197bd65af8c7c6fdf1141d007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c663ff91a07e3cd601ed0a8b92a6df5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa160da2433136fea13b",
+    "url": "/static/css/main.7d87016b.chunk.css"
+  },
+  {
+    "revision": "14231a656f6cffb45bef",
+    "url": "/static/js/2.03247cf4.chunk.js"
+  },
+  {
+    "revision": "fa160da2433136fea13b",
+    "url": "/static/js/main.c932ed9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
